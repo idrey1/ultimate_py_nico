a31631e627a1355cf3e45091505a42c52a27a639
idrey <i.r3y.01@gmail.com>
2024-06-04 17:21:57 -0600

    10. Variables

---
+++ tipos/01-variables.py
@@ -0,0 +1,7 @@
+nombre_curso = "Ultimate Python"
+nombre1 = "Ultimate Python"
+NombreCurso = "Ultimate Python"
+print(nombre_curso, nombre1,NombreCurso)
+alumnos = 5000 #Integer
+puntaje = 9.9  #Float
+publicado = True #Boolean
